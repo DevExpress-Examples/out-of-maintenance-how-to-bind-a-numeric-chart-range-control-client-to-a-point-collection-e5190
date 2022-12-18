c12d3206c9798ee03acf29304f78604d5154df09
DevExpressExampleBot <fakeEmail>
2022-12-18 15:43:19 +0400

    Source auto update [skip ci]

--- VB/BindNumericChartClientToPointCollection/MainWindow.xaml.vb
+++ VB/BindNumericChartClientToPointCollection/MainWindow.xaml.vb
@@ -1,15 +1,12 @@
-﻿Imports System
-Imports System.Collections.Generic
 Imports System.Windows
-Imports System.Windows.Media
 
 Namespace BindNumericChartClientToPointCollection
 
-	Partial Public Class MainWindow
-		Inherits Window
+    Public Partial Class MainWindow
+        Inherits Window
 
-		Public Sub New()
-			InitializeComponent()
-		End Sub
-	End Class
+        Public Sub New()
+            Me.InitializeComponent()
+        End Sub
+    End Class
 End Namespace
